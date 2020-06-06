1783d31690f2247abdc9673cfb3f00a6219d10a3
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-06-07 00:55:13 +0530

    Setting Up

---
+++ documentation/src/components/HomeComp.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+import '../fluent.css';
+
+class Home extends Component{
+  
+  render(){
+    return(
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+
+}
+
+
+export default Home;
